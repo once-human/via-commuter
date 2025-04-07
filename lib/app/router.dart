69f656c05dc90b06b4802e3ae26171b1a08a9a4e
once-human <20220802065@dypiu.ac.in>
2025-04-07 19:31:52 +0530

    feat: update app icon and remove splash screen

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:via_commuter/presentation/screens/splash_screen.dart';
+import 'package:via_commuter/presentation/screens/home_screen.dart';
 
 class AppRouter {
   // Define the routes using GoRouter
   static final List<RouteBase> routes = [
     GoRoute(
       path: '/',
-      builder: (BuildContext context, GoRouterState state) => const SplashScreen(),
+      builder: (BuildContext context, GoRouterState state) => const HomeScreen(),
     ),
-    // Add other routes here
-    // Example:
-    // GoRoute(
-    //   path: '/home',
-    //   builder: (BuildContext context, GoRouterState state) => const HomeScreen(),
-    // ),
   ];
 
   // The old generateRoute method is no longer needed with go_router
