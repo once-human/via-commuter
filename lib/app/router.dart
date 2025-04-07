5476b5c684ffc2608e7696e3b7a4dab0b413489b
once-human <20220802065@dypiu.ac.in>
2025-04-07 11:16:21 +0530

    splash screen update

---
+++ lib/app/router.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:via_commuter/presentation/screens/splash_screen.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+    // Add more routes here as you build
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('No route defined')),
+          ),
+        );
+    }
+  }
+}
